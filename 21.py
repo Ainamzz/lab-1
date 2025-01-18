5bf84b5ec8ea6b9469c9a8873b68c131ea79709c
Ainamkoz <ain.oryngali@kbtu.kz>
2025-01-18 16:51:53 +0500

    la

---
+++ 21.py
@@ -0,0 +1,7 @@
+x = 2
+y = 3.7
+z = 2j
+
+print(type(x))
+print(type(y))
+print(type(z))
---
+++ 22.py
@@ -0,0 +1,11 @@
+x = float(2)
+y = int(3.7)
+z = complex(2)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
